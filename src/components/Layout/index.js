916328dd99fc530b50ef962c68e521f80d2aff0d
Douglaskav <dnmsmpvp@gmail.com>
2020-07-24 09:41:30 -0300

    Stripe Menu

---
+++ src/components/Layout/index.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Layout = styled.div`
+  height: 1005;
+`;
+
+export const Wrapper = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+`;
+
+export default Layout;
